20cc6a07be79becc333298f4d846ac902e168c0e
superlindog <862960632@qq.com>
2021-09-08 10:02:23 +0800

    9.8  9：00

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
   <div class="goods-item" @click="itemClick">
-    <img :src="goodsItem.show.img" alt="" @load="imageLoad" />
+    <img :src="showImage" alt="" @load="imageLoad" />
     <div class="goods-info">
       <p>{{ goodsItem.title }}</p>
       <span class="price">￥{{ goodsItem.price }}</span>
@@ -22,9 +22,16 @@ export default {
       },
     },
   },
+  computed:{
+    showImage(){
+      return this.goodsItem.image || this.goodsItem.show.img
+    }
+  },
   methods: {
     imageLoad() {
-      this.$bus.$emit("itemImageLoad");
+      if(this.$route.path.indexOf('/home')){
+        this.$bus.$emit("itemImageLoad");
+      }
     },
     itemClick() {
       // this.$router.push('/detail/'+ this.goodsItem.iid)
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,16 @@
 <!--  -->
 <template>
   <div id="detail">
-      <detail-nav-bar></detail-nav-bar>
+      <detail-nav-bar class="detail-nav"></detail-nav-bar>
+      <scroll class="content" ref="scroll">
       <detail-swiper :topImages="topImages"></detail-swiper>
       <detail-base-info :goods="goods"></detail-base-info>
-      <detail-shop-info :shop:"shop"></detail-shop-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
+      <goods-list :goods="recommends"></goods-list>
+      </scroll>
   </div>
 </template>
 
@@ -12,9 +18,14 @@
 import DetailNavBar from './childComps/DetailNavBar.vue'
 import DetailSwiper from './childComps/DetailSwiper.vue'
 
-import {getDetail,Goods} from '../../network/detail'
+import {getDetail,Goods,Shop,GoodsParam, getRecommend} from '../../network/detail'
 import DetailBaseInfo from './childComps/DetailBaseInfo.vue'
 import DetailShopInfo from './childComps/DetailShopInfo.vue'
+import Scroll from '../../components/common/scroll/Scroll.vue'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo.vue'
+import DetailParamInfo from './childComps/DetailParamInfo.vue'
+import DetailCommentInfo from './childComps/DetailCommentInfo.vue'
+import GoodsList from '../../components/content/goods/GoodsList.vue'
 
 export default {
 name:"Detail",
@@ -22,14 +33,23 @@ components:{
     DetailNavBar,
     DetailSwiper,
     DetailBaseInfo,
-    DetailShopInfo
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    GoodsList
 },
 data(){
     return{
         id:null,
         topImages:[],
         goods:{},
-        shop:{}
+        shop:{},
+        detailInfo:{},
+        paramInfo:{},
+        commentInfo:{},
+        recommends:[]
     }
 },
 created(){
@@ -45,11 +65,42 @@ created(){
       );
       this.shop=new Shop(data.shopInfo)
 
+      this.detailInfo=data.detailInfo;
+
+      this.paramInfo=new GoodsParam(data.itemParams.info,data.itemParams.rule)
+
+        if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0];
+      }
     })
+
+    getRecommend().then(res=>{
+       this.recommends=res.data.list
+    })
+},
+methods:{
+    imageLoad(){
+        this.$refs.scroll.refresh()
+    }
+},
 }
-}
+
 </script>
 
 <style  scoped>
+#detail{
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+}
+.detail-nav{
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+}
+.content{
+    height: calc(100% - 44px);
 
+}
 </style>
